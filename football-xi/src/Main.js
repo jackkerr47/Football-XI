6f864386220dd7da1552b6d7e8808c3d96c4500a
jackkerr47 <jkerr.47@outlook.com>
2025-07-16 21:57:52 +0100

    First commit

---
+++ football-xi/src/Main.js
@@ -0,0 +1,12 @@
+import Title from './header/Title';
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      <Title />
+    </div>
+  );
+}
+
+export default Main;
---
+++ football-xi/src/header/Title.js
@@ -0,0 +1,12 @@
+import '../Main.css';
+import './Title.css';
+
+function Title() {
+  return (
+    <h1 className="title">
+      Football XI
+    </h1>
+  );
+}
+
+export default Title;
